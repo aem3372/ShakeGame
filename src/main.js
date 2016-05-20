3ea6b6befb31ef44981d5e5bb41bbdf9e7c4286d
fanye <aem3372@163.com>
2016-05-21 04:03:58 +0800

    bugfix

--- src/main.js
+++ src/main.js
@@ -4,17 +4,40 @@ addEventListener('DOMContentLoaded', function () {
     console.log(e);
   }
   
-  var socket = io.connect('http://localhost:8080/sock');
-  socket.on('score', function(data) {
-    console.log(data);
-    document.getElementById('score').innerHTML = data;
-  });
+  lib.Breeze.call('user', 'getNick', {}, function(s) {
+    var nick = s.msg;
+    console.log('nick:' + nick);
+    var socket = io.connect('http://192.168.137.1:8080/sock');
+    socket.on('score', function(data) {
+      console.log(data);
+      document.getElementById('score').innerHTML = data;
+    });
 
-  socket.emit('uploadNick', 'test');
+    socket.emit('uploadNick', nick);
 
-  socket.on('started', function() {
-    setInterval(function() {
-      socket.emit("uploadShake", "shake")
-    }, 1000);
+    socket.on('started', function() {
+      document.getElementById('score').innerHTML = '已经开始';
+      lib.Breeze.addEventListener('BR_SHAKE', function(event) {
+        console.log('shake event');
+        socket.emit("uploadShake", "shake")
+      });
+      lib.Breeze.call('shake', 'open', {}, function(s) {
+
+      }, function(e) {
+        console.log('sersor error');
+      });
+    });
+
+    socket.on('ended', function(data) {
+      for(var i=0; i<3; ++i) {
+        if(data[i].name == nick) {
+          document.getElementById('score').innerHTML = '第' + (i+1) + '名';
+          return ;
+        }
+      }
+      document.getElementById('score').innerHTML = '未上榜';
+    })
+  }, function(e) {
+    console.log('getNick error');
   });
 });
